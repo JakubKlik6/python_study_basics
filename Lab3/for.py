031a428eb94d3f32888f47d4b959ea5c94d3cb4b
Jakub Klik <Jakub.klik6@gmail.com>
2022-07-25 23:18:55 +0200

    Update for.py

--- Lab3/for.py
+++ Lab3/for.py
@@ -22,3 +22,32 @@ if elements[0] == "Error":
     print(elements[1].upper())
 else:
     print(elements[1])
+
+//////////////////////////////////////////
+
+string_A = '+---+---+---+---+'
+string_B = '|   |   |   |   |'
+
+for number in range(9):
+    print(string_A)
+
+print("xxxxxxxxxxxxx")
+
+for number in range(9):
+    if number %2 == 0:
+        print(string_A)
+    else:
+        print(string_B)
+
+print("xxxxxxxxxxxxx")
+
+for number in range(10):
+    print(number * "x")
+
+print("xxxxxxxxxxxxx")
+
+for number in range(10):
+    if number%2 == 0:
+        print(number*"x")
+    else:
+        print(number*"o")
