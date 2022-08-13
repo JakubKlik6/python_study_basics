f5a671da1e033dd2043357dc9d322c2fd2570bf5
Jakub Klik <Jakub.klik6@gmail.com>
2022-08-13 19:32:10 +0200

    Create calendar_time.py

---
+++ Lab3 - advanced types/modules/calendar_time.py
@@ -0,0 +1,16 @@
+import time
+
+print(time.time())
+
+print(time.localtime())
+
+import calendar
+
+print(calendar.month(1998,11))
+
+print(calendar.setfirstweekday(6))
+print(calendar.month(1998,11))
+
+print(calendar.isleap(2000))
+
+print(calendar.month(2019,12))
